b8a49b96934f71cf18e79f81a1a8f456455840b6
ezeanello96 <ezeanello96@gmail.com>
2017-05-16 14:24:56 -0300

    Hecha la conexion con la balanza y muestra el peso en input

--- caramella/views.py
+++ caramella/views.py
@@ -8,12 +8,40 @@ from django.db.models import Q
 from models import *
 import datetime
 import time
+import serial
 
-# Create your views here.
 def index(request):
     return render_to_response('index.html', RequestContext(request))
 
 def cargarLatas(request):
+    if request.is_ajax():
+        if "peso" in request.POST:
+            try:
+                ser = serial.Serial()  # open serial port
+                ser.port = '/dev/ttyUSB0'
+                ser.baudrate = 9600
+                ser.bytesize = serial.EIGHTBITS
+                ser.parity = serial.PARITY_NONE
+                ser.stopbits = serial.STOPBITS_ONE
+                ser.open()
+                print(ser.name)         # check which port was really used
+                ser.write(b'\x05')     # write a string
+                peso = ''
+                for i in range(7):
+                    out = ser.read()
+                    print out
+                    if i==0 or i==8:
+                        peso = peso
+                    elif i==3:
+                        peso = peso+out+"."
+                    else:
+                        peso = peso+out
+                peso = float(peso)
+                print peso
+                ser.close()
+                return JsonResponse({'peso':str(peso)})
+            except:
+                return JsonResponse({'peso':"Estabilice el peso sobre la balanza"})
     return render_to_response('cargarLatas.html', RequestContext(request))
 
 def remito(request):
